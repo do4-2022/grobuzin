04df8dbcc2116046e19f174fad4eea26f13a923a
Nils Ponsard <nilsponsad@gmail.com>
2024-04-08 17:34:03 +0200

    doc: add controller documentation

--- routes/controller.go
+++ routes/controller.go
@@ -2,6 +2,8 @@ package routes
 
 import "gorm.io/gorm"
 
+// Controller for the whole API, all endpoints should be implemented on this struct.
+// This allows to have a single point of access to the database and other services.
 type Controller struct {
 	DB *gorm.DB
 }
