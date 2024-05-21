c169d4078d4f4c1b75fa36051b892c608fc524cc
WoodenMaiden <yann.pomie@laposte.net>
2024-05-21 19:02:05 +0200

    feat: add build timestamp

--- database/function.go
+++ database/function.go
@@ -5,11 +5,12 @@ import (
 )
 
 type Function struct {
-	ID          uuid.UUID `json:"id" ,gorm:"primarykey;type:uuid;default:gen_random_uuid()"`
-	Name        string    `json:"name"`
-	Description string    `json:"description"`
-	Language    string    `json:"language"`
-	Built       bool      `json:"built"` // The builder has built the image for this function
+	ID          	uuid.UUID `json:"id" ,gorm:"primarykey;type:uuid;default:gen_random_uuid()"`
+	Name        	string    `json:"name"`
+	Description 	string    `json:"description"`
+	Language    	string    `json:"language"`
+	Built       	bool      `json:"built"` // The builder has built the image for this function
+	BuildTimestamp 	int64	  `json:"build_timestamp"` // The timestamp of the last build in Unix time
 
 	OwnerID int  `json:"owner_id"`
 	Owner   User `json:"-"`
